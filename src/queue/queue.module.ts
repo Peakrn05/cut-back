554de69a5768e55dc7d54039e47f8a5b572a5131
pp007 <6722771373@g.siit.tu.ac.th.com>
2026-06-08 14:58:51 +0700

    ai fix 008

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { QueueTokenEntity } from './entities/queue-token.entity'
 import { ShopEntity } from '@/shop/entities/shop.entity'
+import { ServiceEntity } from '@/shop/entities/service.entity'
 import { QueueService } from './services/queue.service'
 import { QueueController } from './controllers/queue.controller'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([QueueTokenEntity, ShopEntity])],
+  imports: [TypeOrmModule.forFeature([QueueTokenEntity, ShopEntity, ServiceEntity])],
   providers: [QueueService],
   controllers: [QueueController],
   exports: [QueueService],
--- src/slot/slot.module.ts
+++ src/slot/slot.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { SlotReservationEntity } from './entities/slot-reservation.entity'
 import { ShopEntity } from '@/shop/entities/shop.entity'
+import { ServiceEntity } from '@/shop/entities/service.entity'
 import { SlotService } from './services/slot.service'
 import { SlotController } from './controllers/slot.controller'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SlotReservationEntity, ShopEntity])],
+  imports: [TypeOrmModule.forFeature([SlotReservationEntity, ShopEntity, ServiceEntity])],
   providers: [SlotService],
   controllers: [SlotController],
 })
